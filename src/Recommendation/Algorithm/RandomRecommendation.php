7158fc688e4b4353d9ee00fa54f7815d0e30bac4
musialpatryk <musial.patryk10@gmail.com>
2024-04-14 14:29:33 +0200

    FEAT Implement random recommendation algorithm

--- src/Recommendation/Algorithm/RandomRecommendation.php
+++ src/Recommendation/Algorithm/RandomRecommendation.php
@@ -7,6 +7,8 @@ use App\Recommendation\Interface\RecommendationAlgorithmInterface;
 
 class RandomRecommendation implements RecommendationAlgorithmInterface
 {
+    private const RANDOM_RECOMMENDATIONS_COUNT = 3;
+
     public function getName(): string
     {
         return 'random_recommendation';
@@ -18,6 +20,13 @@ class RandomRecommendation implements RecommendationAlgorithmInterface
      */
     public function recommend(array $recommendable): array
     {
-        // TODO: Implement recommend() method.
+        $randomKeys = array_rand($recommendable, self::RANDOM_RECOMMENDATIONS_COUNT);
+
+        $randomRecommendable = [];
+        foreach ($randomKeys as $key) {
+            $randomRecommendable[] = $recommendable[$key];
+        }
+
+        return $randomRecommendable;
     }
 }
