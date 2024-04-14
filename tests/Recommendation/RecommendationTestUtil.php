6b16194e53c624f168376e5873cb082a0dc3e721
musialpatryk <musial.patryk10@gmail.com>
2024-04-14 17:54:15 +0200

    FEAT Add unit test to CharacterRules algorithm

---
+++ tests/Recommendation/RecommendationTestUtil.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Tests\Recommendation;
+
+use App\Recommendation\Interface\RecommendableInterface;
+
+abstract class RecommendationTestUtil
+{
+    /**
+     * @return string[]
+     */
+    public static function getRecommendableFromTitles(string ...$titles): array
+    {
+        return array_map(
+            static function(string $title): RecommendableInterface {
+                return new class($title) implements RecommendableInterface {
+                    public function __construct(private readonly string $title)
+                    {
+                    }
+
+                    public function getTitle(): string
+                    {
+                        return $this->title;
+                    }
+                };
+            },
+            $titles
+        );
+    }
+}
